95b8debcc2a3c5abc71bf367e209908ed24579f8
SeungjuLee <58246682+jub3907@users.noreply.github.com>
2023-10-18 01:55:09 +0900

    add Match entity

--- src/main/java/duo/gg/server/match/entity/Match.java
+++ src/main/java/duo/gg/server/match/entity/Match.java
@@ -1,5 +1,6 @@
 package duo.gg.server.match.entity;
 
+import duo.gg.server.api.constant.GameModeEnum;
 import duo.gg.server.api.dto.match.MatchApiResult;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -20,6 +21,23 @@ public class Match {
     private String id;
     private String dataVersion;
 
+    private Long gameCreation;
+    private Long gameDuration;
+    private Long gameEndTimestamp;
+    private Long gameId;
+
+    @Enumerated(EnumType.STRING)
+    private GameModeEnum gameMode;
+    private String gameName;
+    private Long gameStartTimestamp;
+    private String gameType;
+    private String gameVersion;
+    private int mapId;
+    private String platformId;
+    private int queueId;
+    private String tournamentCode;
+
+
     @OneToMany(mappedBy = "match", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     private List<Participant> participants = new ArrayList<>();
 
@@ -29,6 +47,19 @@ public class Match {
     public Match(MatchApiResult result) {
         id = result.getMetadata().getMatchId();
         dataVersion = result.getMetadata().getDataVersion();
+        gameCreation = result.getInfo().getGameCreation();
+        gameEndTimestamp = result.getInfo().getGameEndTimestamp();
+        gameDuration = result.getInfo().getGameDuration();
+        gameId = result.getInfo().getGameId();
+        gameMode = result.getInfo().getGameMode();
+        gameName = result.getInfo().getGameName();
+        gameStartTimestamp = result.getInfo().getGameStartTimestamp();
+        gameType = result.getInfo().getGameType();
+        gameVersion = result.getInfo().getGameVersion();
+        mapId = result.getInfo().getMapId();
+        platformId = result.getInfo().getPlatformId();
+        queueId = result.getInfo().getQueueId();
+        tournamentCode = result.getInfo().getTournamentCode();
 
         participants = result.getInfo()
                 .getParticipants()
