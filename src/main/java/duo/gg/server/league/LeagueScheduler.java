787b28a10a10393d6ca7eca28f29205a27b6ddcc
SeungjuLee <58246682+jub3907@users.noreply.github.com>
2023-11-05 17:10:35 +0900

    add ranking scheduler

--- src/main/java/duo/gg/server/league/LeagueScheduler.java
+++ src/main/java/duo/gg/server/league/LeagueScheduler.java
@@ -1,17 +1,22 @@
 package duo.gg.server.league;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class LeagueScheduler {
 
     private final LeagueService service;
 
-    @Scheduled(fixedDelay = 300000)
+    @Transactional
+    @Scheduled(initialDelay = 1000, fixedDelay = 300000)
     public void updateRanking() {
+        log.info("ranking updated");
         service.upsertRanking();
     }
 
