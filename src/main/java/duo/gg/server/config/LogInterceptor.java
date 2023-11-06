feddf52fe6b51803182406a0193da0ba7223b301
SeungjuLee <58246682+jub3907@users.noreply.github.com>
2023-11-07 00:09:13 +0900

    add log interceptor

---
+++ src/main/java/duo/gg/server/config/LogInterceptor.java
@@ -0,0 +1,49 @@
+package duo.gg.server.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.UUID;
+
+@Slf4j
+@Component
+public class LogInterceptor implements HandlerInterceptor {
+
+
+    private static final String LOG_ID = "logId";
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String requestURI = request.getRequestURI();
+        String uuid = UUID.randomUUID().toString();
+
+        request.setAttribute(LOG_ID, uuid);
+
+        // @RequestMappong : HandlerMethod
+        // 정적 리소스 : ResourceHttpRequestHandler
+        if (handler instanceof HandlerMethod) {
+            HandlerMethod hm = (HandlerMethod) handler;
+        }
+
+        log.info("REQUEST [{}][{}][{}]", uuid, requestURI, handler);
+
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        String requestURI = request.getRequestURI();
+        Object uuid = request.getAttribute(LOG_ID);
+
+        log.info("REQUEST [{}][{}][{}]", uuid, requestURI, handler);
+
+        if (ex != null) {
+            log.error("afterCompletion Error!", ex);
+        }
+    }
+}
