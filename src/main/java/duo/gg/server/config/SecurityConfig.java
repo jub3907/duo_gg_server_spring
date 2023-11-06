4ab637fe96b769539a68b461744f508eaa5540ac
SeungjuLee <58246682+jub3907@users.noreply.github.com>
2023-11-07 00:09:47 +0900

    temporarily disable csrf

--- src/main/java/duo/gg/server/config/SecurityConfig.java
+++ src/main/java/duo/gg/server/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -21,8 +22,11 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((authorize) -> authorize
-                .anyRequest().permitAll()
-        );
+                .anyRequest()
+                .permitAll()
+
+        )
+                .csrf(AbstractHttpConfigurer::disable);
 
         return http.build();
     }
