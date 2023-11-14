622928d2ee7e63e6edcbda197f4b034d7dbe2d7a
SeungjuLee <58246682+jub3907@users.noreply.github.com>
2023-11-14 12:43:50 +0900

    modify controller to restcontroller

--- src/main/java/duo/gg/server/post/PostController.java
+++ src/main/java/duo/gg/server/post/PostController.java
@@ -10,15 +10,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Slf4j
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class PostController {
     private final PostService service;
