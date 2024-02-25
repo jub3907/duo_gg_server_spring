a1b97707ff0ce3560e5bccf50280d841c1fb0de2
SeungjuLee <58246682+jub3907@users.noreply.github.com>
2024-02-25 14:41:39 +0900

    Update SecurityConfig.java

--- src/main/java/duo/gg/server/config/SecurityConfig.java
+++ src/main/java/duo/gg/server/config/SecurityConfig.java
@@ -1,11 +1,15 @@
 package duo.gg.server.config;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -27,6 +31,22 @@ public class SecurityConfig implements WebMvcConfigurer {
                         "https://172.31.5.116:3000")
                 .allowedMethods("GET", "POST", "DELETE");
     }
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilterRegistrationBean(){
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(false);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.setMaxAge(6000L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        FilterRegistrationBean<CorsFilter> filterBean = new FilterRegistrationBean<>(new CorsFilter(source));
+        filterBean.setOrder(0);
+
+        return filterBean;}
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
